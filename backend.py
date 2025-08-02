bb5a4a548818b397f6f7c53d2a4c62795a216f80
Ankur Srivastava <srivastavaankur987@gmail.com>
2025-08-02 18:56:48 +0530

    commit

---
+++ backend.py
@@ -0,0 +1,36 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = ChatOpenAI(
+    model_name="google/gemma-3n-e4b-it",
+    openai_api_key=os.getenv("OPENAI_API_KEY"), 
+    openai_api_base=os.getenv("OPENAI_API_BASE"), 
+    max_tokens=50
+)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
